59440b2ba75983e1ca3659442b69646ba45aaa3e
Soulis-Alex <soulis.alex@outlook.com>
2021-09-09 16:05:00 +0200

    Ajout d'un bloc texte

--- popup.js
+++ popup.js
@@ -1,12 +1,11 @@
 
 getLocalStorage();
 
-
 let newText;
 function getCurrentTab() {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tab) {
     chrome.tabs.sendMessage(tab[0].id, { greeting: "hello" }, function (response) {
-      newText = response;
+      newText = newText + " " + response;
       document.getElementById("textarea").innerHTML = newText;
       setLocalStorage(newText);
     });
@@ -14,21 +13,19 @@ function getCurrentTab() {
 }
 
 let boutonAction = document.getElementById("action");
-console.log(boutonAction);
 boutonAction.addEventListener("click", function () {
   getCurrentTab();
 });
 
 
-// Local Storage in progress
+
 function setLocalStorage(value) {
   chrome.storage.local.set({ key: value });
-  //console.log("NewText " + typeof newText);
 }
 function getLocalStorage() {
   chrome.storage.local.get(['key'], function (result) {
-    console.log(result.key);
-    document.getElementById("textarea").innerHTML = result.key;
+    newText = result.key
+    document.getElementById("textarea").innerHTML = newText;
   });
 }
 
