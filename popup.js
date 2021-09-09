52f5577f0cd021d45d38e71746485506d0d3d630
Noémie Ragot <noemieprog@gmail.com>
2021-09-09 09:46:30 +0200

    Récup text

--- popup.js
+++ popup.js
@@ -1,27 +1,24 @@
+
  function getCurrentTab(){
     chrome.tabs.query({active: true, currentWindow: true}, function(tab) {
     chrome.tabs.sendMessage(tab[0].id, {greeting: "hello"}, function(response) {
-      console.log(response.farewell);
-      console.log(tab);
+      console.log(response);
+      newText = response;
+      console.log(newText);
+      return newText;
     });
   });
 }
-
+let newText;
 let boutonAction=document.getElementById("action");
 console.log(boutonAction);
 boutonAction.addEventListener("click", function(){
   getCurrentTab();
+
 });
 
-
-  /*chrome.tabs.getSelected(null, function(tab) {
-    // Send a request to the content script.
-    chrome.tabs.sendRequest(tab.id, {action: "getDOM"}, function(response) {
-
-      console.log(response.dom);
-    });
-  });   
-});*/
+console.log(newText);
+document.getElementById("selectedText").innerHTML = newText; 
 
 
 
--- script.js
+++ script.js
@@ -10,32 +10,50 @@
 
    console.log("test");
 
-   chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      console.log(sender.tab ?
-                  "from a content script:" + sender.tab.url :
-                  "from the extension");
-      if (request.greeting == "hello")
-        sendResponse({farewell: "goodbye"});
-    }
-  );
-
-
-/*function getSelectionText() {
-    console.log("step1");
-    if (window.getSelection) {
-        console.log("step2");
-      //only work if supported
-      var selection = window.getSelection(); //get the selection object
-      alert(
-        "Selected text: \n" + selection.toString()
-      );
-    }
+function getSelectionText() {
+  if (window.getSelection) {
+    //only work if supported
+    var selObj = window.getSelection();
+    console.log(selObj);
+    var selectedText = selObj.toString();
+    console.log(selectedText); 
+    return selectedText;
+    //get the selection object
+    //alert(
+      //"Selected text: \n" + selection.toString()
+    //);
   }
+}
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    
+    if (request.greeting == "hello")
+      sendResponse(getSelectionText());
+      
+  }
+);
+
+document.getElementById
 
 
-  getSelectionText();
-    alert("test");
+
+
+
+ 
     // paste dans la pop up
-*/
 
+// chrome.runtime.onMessage.addListener(
+//     function(request, sender, sendResponse) {
+//       console.log(sender.tab ?
+//                   "from a content script:" + sender.tab.url :
+//                   "from the extension");
+//       if (request.greeting == "hello")
+//         sendResponse({farewell: "goodbye"});
+//     }
+//   );
+
+
+
+// getSelectionText();
+//   alert("test");
