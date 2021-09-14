beb7e0117b7ac6937e76e16fb3ab96249ca6651b
Soulis-Alex <soulis.alex@outlook.com>
2021-09-14 11:38:44 +0200

    Ajout de la fonctionnalité Save

--- popup.js
+++ popup.js
@@ -1,14 +1,17 @@
-
 getLocalStorage();
 
 let newText;
 function getCurrentTab() {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tab) {
-    chrome.tabs.sendMessage(tab[0].id, { greeting: "hello" }, function (response) {
-      newText = newText + " " + response;
-      document.getElementById("textarea").innerHTML = newText;
-      setLocalStorage(newText);
-    });
+    chrome.tabs.sendMessage(
+      tab[0].id,
+      { greeting: "hello" },
+      function (response) {
+        newText = newText + " " + response;
+        document.getElementById("textarea").value = newText;
+        setLocalStorage(newText);
+      }
+    );
   });
 }
 
@@ -17,24 +20,31 @@ boutonAction.addEventListener("click", function () {
   getCurrentTab();
 });
 
+let boutonClear = document.getElementById("clear");
+boutonClear.addEventListener("click", function () {
+  chrome.storage.local.clear();
+  newText = "";
+  document.getElementById("textarea").value = "";
+});
+
+/*let textAreaInput = document.getElementById("textarea");
+textAreaInput.addEventListener("change", function (e) {
+  setLocalStorage(e.target.value);
+});*/
+
+
+let boutonSave = document.getElementById("save");
+boutonSave.addEventListener("click", function () {
+  setLocalStorage(document.getElementById("textarea").value);
+});
 
 
 function setLocalStorage(value) {
   chrome.storage.local.set({ key: value });
 }
 function getLocalStorage() {
-  chrome.storage.local.get(['key'], function (result) {
-    newText = result.key
-    document.getElementById("textarea").innerHTML = newText;
+  chrome.storage.local.get(["key"], function (result) {
+    newText = result.key === undefined ? "" : result.key;
+    document.getElementById("textarea").value = newText;
   });
 }
-
-
-
-
-
-
-
-
-
-
